5f58eab13eaea4670aa4feaa04d3647fb903d506
ely211 <gonzalesm405@gmail.com>
2017-07-07 16:51:20 -0500

    muestra

--- src/assets/js/components/top-news.js
+++ src/assets/js/components/top-news.js
@@ -7,9 +7,7 @@ function Noticia(title, brief, url, classNews){
   this.classNews = classNews;
 }
 
-const newsState = {
-    top: []
-}
+const topNew = [];
 
 const content = (titleNews, briefNews, urlImg, classContainer)=>{
   let container = $('<section class="'+classContainer+' top__content"></section>');
@@ -24,34 +22,26 @@ const content = (titleNews, briefNews, urlImg, classContainer)=>{
   return container;
 }
 
-
-
-$.get("/api/news/", (response, status)=>{
-  $.each(response, (i, data)=>{
-    console.log(response);
-    $.each(data.categories, (i, categorie)=>{
-      console.log(categorie);
-      if(categorie == 0){
-        const aNews = new Noticia(data.title, data.brief, data.img, 'col-xs-12 col-sm-12 has-feedback');
-        newsState.top.push(aNews);
-      }
-    })
-  })
-});
-
 const sectionTopNews = (update)=>{
   let topNews = $('<section class="container-fluid top"></section>');
   let row = $('<section class="row"></div>');
 
-  // console.log(newsState);
+  $.get("/api/news/", (response, status)=>{
+    console.log(response);
+    $.each(response, (i, data)=>{
+      $.each(data.categories, (i, categorie)=>{
+        if(categorie == 0){
+          const aNews = new Noticia(data.title, data.brief, data.img, 'col-xs-12 col-sm-12 has-feedback');
+          topNew.push(aNews);
+        }
+      })
+    })
+    console.log(topNew.length);
 
-  $.each(newsState, (i, noticia)=>{
-    console.log('newsState-tip');
-    console.log(noticia);
-    $.each(noticia, (i, info)=>{
-      console.log('info');
-      let itemNews = content(info.title, noticia.brief, 'assets/img/news/' + noticia.url, noticia.classNews);
-      console.log(itemNews);
+    $.each(topNew, (i, noticia)=>{
+      console.log('noticia get');
+      console.log(noticia);
+      let itemNews = content(noticia.title, noticia.brief, 'assets/img/news/' + noticia.url, noticia.classNews);
       row.append(itemNews);
     })
   });
